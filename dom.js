12cd92845485277990582ce911548e22de794131
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-01-27 13:32:35 +0100

    Added teacher picture and moved html files to pages folder.

--- dom.js
+++ dom.js
@@ -1,28 +1,22 @@
 /*******************************************************************************/
 // DOM Manipulation
 /*******************************************************************************/
-//     Create card function with couse as argument
 const createCard = (course) => {
     console.log(course);
-//     Create card/div for each course
     const div = document.createElement('div');
-    // Add class to card
-    div.classList.add('course-image');
-    // Add content: image. We need to create an image function!
+    div.classList.add('course-image', 'img');
     div.appendChild(createImage(course.imageUrl, course.id));
-    // Add content: title and info. We need to create a function that renders title and info function!
     div.appendChild(createCourseInfo(course));
-
+    
     return div;
 }
 
-// The function to render the image
-// Arguments are the image url and the id of the course
 const createImage = (imageUrl, id) => {
     console.log(imageUrl);
     // We save the image in a variable called image
     const image = document.createElement('img');
-    // Set the source of the image
+    // Set the source of the image. 
+    // If CHANGE the position of the image files, EDIT the source here.
     image.setAttribute('src', `${imageUrl}`);
     // Set the data-id of the image
     image.setAttribute('id', id);
@@ -32,7 +26,8 @@ const createImage = (imageUrl, id) => {
 
 const createCourseInfo = (course) => {
     const paragraph = document.createElement('p');
-    paragraph.appendChild(document.createTextNode(`${course.name} - ${course.info}`));
+    paragraph.appendChild(document.createTextNode(`${course.name} - ${course.duration} - Rating: ${course.averageRating}/5`));
+    
     // courseInfo.classList.add('course-info');
     return paragraph;
 };
