13c80871d41341636a6bf4db9c82af9d98972a14
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-01-28 17:52:55 +0100

    Starting Add course JS code. Fixing HTML footer.

---
+++ scripts/course.js
@@ -0,0 +1,23 @@
+import HttpClient from "./http.js";
+import { convertFormDataToJson } from "./utilities.js";
+
+const form = document.querySelector('addCourseForm');
+
+const addCourse = async (e) => {
+    e.preventDefault();
+    // Get data from the form, and send it to the server through an object
+    const course = new FormData(form);
+    const object = convertFormDataToJson(course);
+    saveCourse(object);
+};
+
+const saveCourse = async (course) => {
+    // Save data to db.json
+    const url = 'http://localhost:3000/courses';
+    const http = new HttpClient(url);
+    await http.add(course);
+    console.log(course);
+    // Redirect to courses.html once the data is added to check if the addition was successful
+    location.href = './courses.html';
+};
+form.addEventListener('submit', addCourse);
