b06c0ed8a9d42d4ee0a047313cd200ca16b91c0a
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-01-28 18:31:44 +0100

    Created student.js to add students. Not working yet.

---
+++ scripts/student.js
@@ -0,0 +1,22 @@
+import HttpClient from "./http.js";
+import { convertFormDataToJson } from "./utilities.js";
+
+const form = document.querySelector('addStudentForm');
+const studentsGallery = document.querySelector('#students-gallery');
+
+const addStudent = async (e) => {
+    e.preventDefault();
+    const student = new FormData(form);
+    const object = convertFormDataToJson(student);
+    saveStudent(object);
+};
+
+const saveStudent = async (student) => {
+    const url = 'http://localhost:3000/students';
+    const http = new HttpClient(url);
+    await http.add(student);
+    console.log(student);
+    location.href = './students.html';
+};
+
+form.addEventListener('submit', addStudent);
