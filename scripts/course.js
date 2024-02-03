a0cab7e8911681df7d11484d1a1b5d153e9f2243
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-02-03 20:28:29 +0100

    Editing course.js to enable the save button, connecting it to db.json.

--- scripts/course.js
+++ scripts/course.js
@@ -1,16 +1,28 @@
 import HttpClient from "./http.js";
 import { convertFormDataToJson } from "./utilities.js";
+import { createCard } from "./dom.js";
+// Add a course
 
+const course = {
+    id: '111',
+    name: 'Javascript',
+    duration: '12 weeks',
+    teacherId: 7001,
+    averageRating: 5,
+    imageUrl: 'Javascript.jpg'
+}
+console.log(course);
 const form = document.querySelector('addCourseForm');
-
-const addCourse = async (e) => {
+const createCourse = async (e) => {
     e.preventDefault();
     // Get data from the form, and send it to the server through an object
     const course = new FormData(form);
     const object = convertFormDataToJson(course);
     console.log(object);
+    // Save data to db.json
     saveCourse(object);
 };
+console.log(form);
 
 const saveCourse = async (course) => {
     // Save data to db.json
@@ -20,4 +32,4 @@ const saveCourse = async (course) => {
     // Redirect to courses.html once the data is added to check if the addition was successful
     location.href = './index.html';
 };
-form.addEventListener('submit', addCourse);
+// form.addEventListener('submit', addCourse);
