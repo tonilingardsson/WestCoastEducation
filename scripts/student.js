e93eef5832169060e821d31732619905bc5fd87e
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-02-14 18:26:08 +0100

    Students are now saved, matching the data structure on the db.json file. HTML attribute name defines the key in the object.

--- scripts/student.js
+++ scripts/student.js
@@ -1,23 +1,31 @@
 import HttpClient from "./http.js";
 import { convertFormDataToJson } from "./utilities.js";
+// Add a student
 
-const form = document.querySelector('addStudentForm');
-const studentsGallery = document.querySelector('#students');
-
-const addStudent = async (e) => {
+const form = document.querySelector('#addStudentForm');
+const createStudent = async (e) => {
     e.preventDefault();
+    // Get data from the form, and send it to the server through an object
     const student = new FormData(form);
     const object = convertFormDataToJson(student);
-    console.log(student);
+     // Populate a message that the student was added to the db.json
+     const message = `The student ${student.name} was added to db.json`;
+     console.log(message);
+     alert(message);
+     // Save data to db.json
     saveStudent(object);
 };
+console.log(form);
 
 const saveStudent = async (student) => {
+    // Save data to db.json
     const url = 'http://localhost:3000/students';
     const http = new HttpClient(url);
     await http.add(student);
-    console.log(student);
+   
+
+    // Redirect to students.html once the data is added to check if the addition was successful
     location.href = './index.html';
 };
 
-form.addEventListener('submit', addStudent);
+form.addEventListener('submit', createStudent);
