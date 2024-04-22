af4e140d23db6d9a5b10f75d7b5ac3df7cbb6a2c
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-04-22 10:50:56 +0200

    Created dynamic HTML header and footer.

--- app.js
+++ app.js
@@ -4,20 +4,11 @@ const courseContainer = document.querySelector('#courses-gallery');
 
 const initApp = async () => {
 
-    const path = location.pathname;
     const courses = await getAllCourses();
 
-    if (path.includes('info')) {
-        const query = location.search.split('=')[1];
-        console.log(query);
-        const courseById = await getCourseById(query);
-        if (courseById) courseContainer.appendChild(createCard(courseById));
-    } else {
-        courses.forEach(course => {
-            courseContainer.appendChild(createCard(course));
-        });
-    }
-
+    courses.forEach(course => {
+        courseContainer.appendChild(createCard(course));
+    });
 
 }
 
